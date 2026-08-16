ccd21932898fc0e51efc97050abe4a6c0b89ecdf
fcwjhrwp96-sys <fcwjhrwp96@privaterelay.appleid.com>
2026-08-16 17:37:58 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,52 @@
-const CACHE = "rudram-motors-v1";
+const CACHE = "rudram-motors-v2";
+
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon.svg",
+  "./favicon.svg",
+  "./offline.html"
+];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE).then(cache =>
-      cache.addAll(["./", "./index.html", "./manifest.json"])
-    )
+    caches.open(CACHE)
+      .then(cache => cache.addAll(APP_FILES))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
   event.respondWith(
-    caches.match(event.request).then(response =>
-      response || fetch(event.request)
-    )
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+
+        caches.open(CACHE).then(cache => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      })
+      .catch(() =>
+        caches.match(event.request).then(cached => {
+          return cached || caches.match("./offline.html");
+        })
+      )
   );
 });
